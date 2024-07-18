e40d90d7204353662a33485bc92b4b4d479d2e81
matvey033 <mat.gol20003@gmail.com>
2024-07-18 08:58:06 +0300

    first project commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://i.ytimg.com/vi/culwxTEFMYg/maxresdefault.jpg' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+          <img src='https://s1.1zoom.me/big3/446/375561-svetik.jpg' />
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            posts
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile;
