03bf67c305cac9c3f53927457663e17c1b306def
matvey033 <mat.gol20003@gmail.com>
2024-07-19 11:23:23 +0300

    new component - Dialogs

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
+      <div className='content'>
+        <Dialogs />
+      </div>
       {/* <Profile /> */}
     </div>
   );
